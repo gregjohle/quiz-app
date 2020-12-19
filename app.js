295417a5ef35509112d2ce0c7a61ba9f014e6884
Greg Johle <greg.johle@gmail.com>
2020-12-19 16:15:34 -0500

    functionality improvements

--- app.js
+++ app.js
@@ -34,16 +34,6 @@ const store = {
             correctAnswer: 'Stereolithography'
         },
         {
-            question: 'The technical term for 3D printing is what?',
-            answers: [
-                '3D Printing',
-                'Additive CNC',
-                'Stereolithography',
-                'Stereolycanthropy'
-            ],
-            correctAnswer: 'Stereolithography'
-        },
-        {
             question: 'How many axis does a 3D printer utilize?',
             answers: [
                 '1',
@@ -69,40 +59,271 @@ const store = {
     score: 0
 };
 
-// When start is clicked, the first question should load, along with the number correct and question number
-
-function clear
-
-function generateQuestion(question) {
-    return `
-    
-  `
+// This clears the Div for formatting
+function clearHero() {
+    $('.hero').html('');
 };
 
-function render() {
+// This formats the HTML for insertion
+function formatHero() {
+    var currentQuestionNumber = `<h1>3D Printing Facts</h1>
+    <div class="indexInfo">
+        <h3 class="currQuestNum"></h3>
+        <h3>of 5</h3>
+    </div>
+    <div class="scoreInfo">
+    <h3 class="numberCorrect"></h3>
+    <h3>Correct</h3>
+    </div>`
+
+    $('.info').append().html(currentQuestionNumber);
+
+    $('.hero').html(`
+        <p class="currentQuestion"></p>
+        <form id="question">
+            <ul>
+                <li>
+                    <input type="radio" id="ans1" name="ans" value="" required>
+                    <label for="ans1" class="ans1"></label>
+                </li>
+                <li>
+                    <input type="radio" id="ans2" name="ans" value="" required>
+                    <label for="ans2" class="ans2"></label>
+                </li>
+                <li>
+                    <input type="radio" id="ans3" name="ans" value="" required>
+                    <label for="ans3" class="ans3"></label>
+                </li>
+                <li>
+                    <input type="radio" id="ans4" name="ans" value="" required>
+                    <label for="ans4" class="ans4"></label>
+                </li>
+            </ul>
+        </form>
+        <button type="submit" id ="submitAnswer" form="question">Submit</button>    
+    `);
+}
+
+// This inserts the current question string into the div p
+function generateQuestion() {
+    var qHero = store.questions[store.questionNumber].question;
+
+    $('.currentQuestion').append(qHero);
+};
+
+// This parses the current question answers and inserts them into the div form
+function generateAnswers() {
+    var ansOne = store.questions[store.questionNumber].answers[0];
+    var ansTwo = store.questions[store.questionNumber].answers[1];
+    var ansThree = store.questions[store.questionNumber].answers[2];
+    var ansFour = store.questions[store.questionNumber].answers[3];
+
+    $('input[id="ans1"]').val(ansOne);
+    $('label[for="ans1"]').append(ansOne);
+
+    $('input[id="ans2"]').val(ansTwo);
+    $('label[for="ans2"]').append(ansTwo);
+
+    $('input[id="ans3"]').val(ansThree);
+    $('label[for="ans3"]').append(ansThree);
+
+    $('input[id="ans4"]').val(ansFour);
+    $('label[for="ans4"]').append(ansFour);
 
 };
 
-// each question should be multiple choice
+// This inserts the question number into the header
+function generateQuestionNumber() {
+    var currQuest = store.questionNumber + 1;
+    $('.currQuestNum').replaceWith(currQuest);
+};
 
+// This inserts the current score into the header
+function generateScore() {
+    var currScore = store.score;
+    $('.numberCorrect').replaceWith(currScore);
+};
+
+function clearScoreAndQuestionNumber() {
+    var currentQuestionNumber = `<h1>3D Printing Facts</h1>
+    <div class="indexInfo">
+        <h3 class="currQuestNum"></h3>
+        <h3>of 5</h3>
+    </div>
+    <div class="scoreInfo">
+    <h3 class="numberCorrect"></h3>
+    <h3>Correct</h3>
+    </div>`
+
+    $('.info').append().html(currentQuestionNumber);
+}
+
+// This starts the quiz
+function startQuiz() {
+    store.quizStarted = true;
+}
+
+// This renders the page 
+function renderQuest() {
+    clearHero();
+    formatHero();
+    generateQuestionNumber();
+    generateScore();
+    generateQuestion();
+    generateAnswers();
+};
+
+// This starts the quiz with the first button is pressed
+function letsBegin() {
+    $('.beginQuiz').on('click', event => {
+        console.log('The Quiz is starting');
+        startQuiz();
+        renderQuest();
+    });
+};
 // after a choice is made and the submit button is pushed, there should be feedback if it was the correct answer or not
 
 // if the answer is correct, the number correct should increase
 
-function scoreQeustion() {
-
+// This dieplays a correct result
+function renderCorrect() {
+    clearScoreAndQuestionNumber();
+    generateQuestionNumber();
+    generateScore();
+    $('.hero').html(`
+        <p class="right">You are correct</p>
+        <button class="nextQuestion">
+                <span>Continue</span>
+            </button>
+    `);
 };
 
-// there should be a next button
+// This formats for the incorrect result
+function formatIncorect() {
+    $('.hero').html(`
+        <p class="wrong">You are not correct</p>
+        <p>The Correct answer is</p>
+        <p class="rightAns"></p>
+        <button class="nextQuestion">
+                <span>Continue</span>
+            </button>
+    `);
+};
 
-// the next button should load the next question
+// This renders the incorrect result, and informs of the correct result
+function renderIncorrect() {
+    formatIncorect();
+    clearScoreAndQuestionNumber();
+    generateQuestionNumber();
+    generateScore();
+    $('.rightAns').append(store.questions[store.questionNumber].correctAnswer);
+}
+
+function indexQuestion() {
+    store.questionNumber = store.questionNumber + 1;
+};
+
+function renderEnd() {
+    $('.hero').html(`
+        <p>Congratulations! You have completed the quiz!</p>
+        <p>I hope you have learned something new about 3D Printing.</p>
+        <p>Would you like to take the quiz again?</p>
+        <button class="startOver">
+                <span>Start Over?</span>
+            </button>
+    `);
+};
+
+function nextQuestion() {
+    $('.hero').on('click', '.nextQuestion', event => {
+        var endOfQuiz = store.questions.length - 1;
+        var currNum = store.questionNumber;
+        if (currNum < endOfQuiz) {
+            console.log('Moving on');
+            indexQuestion();
+            renderQuest();
+        };
+        if (endOfQuiz === currNum) {
+            clearHero();
+            clearScoreAndQuestionNumber();
+            generateQuestionNumber();
+            generateScore();
+            renderEnd();
+        };
+    });
+};
+
+function scoreIncrease() {
+    store.score += 1;
+};
+
+function renderResult() {
+    // listen for the submit
+    $('.hero').submit(function(event) {
+        // If correct, score and render result
+        event.preventDefault();
+        var eAns = $('input[name="ans"]:checked').val();
+        var cAns = store.questions[store.questionNumber].correctAnswer;
+        if (eAns === cAns) {
+            scoreIncrease();
+            clearScoreAndQuestionNumber();
+            generateScore();
+            generateQuestionNumber();
+            renderCorrect();
+        }
+        if (eAns != cAns) {
+            renderIncorrect();
+        }
+    });
+};
+
+function renderNewQuiz() {
+    $('.info').html(`
+    <h1>3D Printing Facts</h1>
+    `);
+    $('.hero').html(`
+    <h3>Welcome to the Quiz!</h3>
+    <p>This is a short quiz to test your knowledge of 3D printing. When you are ready to begin, press the button below!</p>
+    <button class="beginQuizAgain">
+        <span>START</span>
+    </button>
+    `);
+};
+
+function resetQuizValues() {
+    store.quizStarted = false;
+    store.questionNumber = 0;
+    store.score = 0;
+}
+
+function startOver() {
+    $('.hero').on('click', '.startOver', event => {
+        resetQuizValues();
+        renderNewQuiz();
+        console.log(store.quizStarted);
+        console.log(store.questionNumber);
+        console.log(store.score);
+    });
+};
+
+function beginAgain() {
+    $('.beginQuizAgain').on('click', event => {
+        console.log('The Quiz is starting');
+        startQuiz
+        renderQuest();
+    });
+};
 
 // when done, the final score should be shown, and a button to retake the quiz
 
 
 
 $(function() {
-
+    letsBegin();
+    nextQuestion();
+    renderResult();
+    startOver();
+    beginAgain();
 });
 
 /**
@@ -130,4 +351,4 @@ $(function() {
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
-// These functions handle events (submit, click, etc)
+/** These functions handle events (submit, click, etc) **/
